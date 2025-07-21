19b0be6a94e02e85f3b73a8e533060db85d3b5d6
Hansimov <591172499@qq.com>
2025-07-22 04:48:01 +0800

    :gem: [Feature] New Emailer: connect, content_to_mime_msg, and send

--- src/acto/__init__.py
+++ src/acto/__init__.py
@@ -2,3 +2,4 @@ from .times import PatternedDatetimeSeeker, UnitTimeDistConverter
 from .periods import Perioder
 from .log import ActionLogger
 from .retry import Retrier
+from .email import Emailer, EmailConfigsType, EmailContentType
